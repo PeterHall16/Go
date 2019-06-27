153de37e6b0e3a16385f49ba079c734f1b5aff42
Peter Hall <peterhall330@outlook.com>
2019-06-27 10:56:50 -0400

    Updates

--- File Server/Main.go
+++ File Server/Main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
+
+const port = ":3000"
 
 func sayHello(w http.ResponseWriter, r *http.Request) { // Create Handler
 	message := r.URL.Path // Get the message from the URL
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello " + message
 	w.Write([]byte(message))
+
 }
+
 func main() {
 	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Running on port " + port)
+
+	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
+
 }
